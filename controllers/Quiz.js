3905de09e845e7b9a812a5ce482a6058bbce8cc9
Ankit <ankitcha810@gmail.com>
2023-10-05 12:56:23 +0530

    Final submit route added

--- controllers/Quiz.js
+++ controllers/Quiz.js
@@ -9,7 +9,6 @@ exports.postDrData = async (req, res) => {
       city: city,
       state: state,
     });
-  
     try {
       await newDoctor.save();
       return res.status(201).json({ message: 'Doctor data inserted' });
@@ -18,7 +17,6 @@ exports.postDrData = async (req, res) => {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   };
-
 
   exports.getDoctorName = async (req, res) => {
     try {
@@ -47,6 +45,45 @@ exports.postDrData = async (req, res) => {
     } catch (error) {
       console.error("Error fetching data:", error);
       res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+  
+  exports.handleUserQuizSubmit = async (req, res) => {
+    const userId = req.body.userId;
+    const totalPoints = req.body.totalPoints;
+    const categoryName = req.body.categoryName;
+  
+    try {
+      const user = await Quiz.findById(userId);
+      
+      if (!user) {
+        return res.status(401).json({ msg: "User not found" });
+      }
+  
+      if (user.QuizCategory && user.QuizCategory[categoryName]) {
+        const category = user.QuizCategory[categoryName];
+  
+        if (category.isPlayed) {
+          // Category has already been played, return an error message
+          return res.status(400).json({ msg: "Category already played" });
+        }
+  
+        // Update the category as played and set the total points
+        category.isPlayed = true;
+        category.TotalPoints = totalPoints;
+  
+        await user.save();
+  
+        return res.status(200).json({
+          msg: "QuizCategory updated successfully",
+          updatedUser: user
+        });
+      } else {
+        return res.status(404).json({ msg: "Category not found" });
+      }
+    } catch (error) {
+      console.log("error: ", error);
+      return res.status(400).json({ msg: "Internal Server Error" });
     }
   };
   
--- routes/Quiz.js
+++ routes/Quiz.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
 
-const {postDrData, getDoctorName , getOnlyName} = require("../controllers/Quiz")
+const {postDrData, getDoctorName , getOnlyName , handleUserQuizSubmit} = require("../controllers/Quiz")
 
 router.post("/user" , postDrData);
 router.get("/get/docter/name" , getDoctorName);
 router.get("/get/names" , getOnlyName);
+router.post("/submit/score" , handleUserQuizSubmit);
 
 
 
